ac43a5cd5dcb98a14a299f22f323a8d1ba53375c
gidambayiantony <gidambayiantony@gmail.com>
2023-06-22 05:19:53 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+  int n;
+
+  n = 98;
+  printf("%d\n", n);
+  n = 402;
+  printf("%d\n", n);
+  n = 1024;
+  printf("%d\n", n);
+  n = 0;
+  printf("%d\n", n);
+  n = -98;
+  printf("%d\n", n);
+  return (0);
+}
+
+
